23cff289184375a3279d911f251003f03150faf6
Anonymous20XXcodes <78462915+Anonymous20XXcodes@users.noreply.github.com>
2021-02-09 20:26:15 +1100

    Update models.py

--- models.py
+++ models.py
@@ -2,9 +2,9 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 from torch_geometric.nn import MessagePassing
-from torch_geometric.nn import GCNConv, ChebConv  # noqa
+from torch_geometric.nn import GCNConv, ChebConv
 
-
+# The baseline method SAT can be found at https://github.com/xuChenSJTU/SAT-master-online
 
 class MLP(torch.nn.Module):
     def __init__(self, input_dim,  hidden_dim, output_dim, feature_dim = None,
@@ -26,7 +26,6 @@ class MLP(torch.nn.Module):
         if self.feature_pre:
             x = self.linear_pre(x)
         x = self.linear_first(x)
-        # x = F.relu(x)
         if self.dropout:
             x = F.dropout(x, training=self.training)
         for i in range(self.layer_num - 2):
@@ -35,28 +34,6 @@ class MLP(torch.nn.Module):
             if self.dropout:
                 x = F.dropout(x, training=self.training)
         x = self.linear_out(x)
-        # x = F.normalize(x, p=2, dim=-1)
-        return x
-
-class ESAGE(MessagePassing):
-    def __init__(self, input_dim, hidden_dim, output_dim, layer_num=2, conv_num = 5, alpha=0.9):
-        super(ESAGE, self).__init__(aggr='max')
-        
-        self.input_dim = input_dim
-        self.encoder = MLP(self.input_dim, hidden_dim, output_dim, layer_num = layer_num)
-        self.conv_num = conv_num
-        self.output_dim = output_dim
-        self.hidden_dim = hidden_dim
-        
-    def sage_msg(self, x, edge_index):
-        de_X = x
-        x_j = self.propagate(edge_index, x=de_X)
-        return torch.max(de_X,x_j) 
-
-    def sage_forward(self, x, edge_index):
-        x = self.encoder(x)
-        for i in range(self.conv_num):
-            x = self.sage_msg(x, edge_index)
         return x
 
 
@@ -68,8 +45,6 @@ class KSNN(MessagePassing):
         self.input_dim = input_dim
         self.encoder = MLP(self.input_dim, hidden_dim, output_dim, layer_num = layer_num)
         self.decoder = MLP(output_dim, hidden_dim, self.input_dim, layer_num = layer_num)     
-#         self.encoder = nn.Linear(self.input_dim,output_dim,bias=False)
-#         self.decoder = nn.Linear(output_dim,self.input_dim,bias=False)
         self.alpha = alpha
         self.conv_num = conv_num
         self.output_dim = output_dim
@@ -93,7 +68,6 @@ class KSNN(MessagePassing):
     def rpca(self, X, edge_index):
         x = self.pca_Z if not self.pca_Z is None else self._set_pcaX(X)
         for i in range(self.conv_num):
-            # x = F.relu(self.rpca_msg(x,edge_index))
             x = self.rpca_msg(x,edge_index)
         return x
 
@@ -127,38 +101,6 @@ class KSNN(MessagePassing):
     def pca(self, X, edge_index):
         x = self.pca_Z if not self.pca_Z is None else self._set_pcaX(X)
         for i in range(self.conv_num):
-            # x = F.relu(self.pca_msg(x,edge_index))
             x = self.pca_msg(x,edge_index)
         return x
     
-
-
-
-class Naive(MessagePassing):
-    def __init__(self, input_dim, hidden_dim, output_dim, layer_num=2, conv_num = 5, alpha=0.9):
-        super(Naive, self).__init__() #aggr='max'
-        self.input_dim = input_dim
-#         self.m_encoder = MLP(self.input_dim, hidden_dim, output_dim, layer_num = enc_layer_num)
-#         self.m_decoder = MLP(output_dim, hidden_dim, self.input_dim, layer_num = enc_layer_num)
-        self.encoder = MLP(self.input_dim, hidden_dim, output_dim, layer_num = layer_num)
-        self.decoder = MLP(output_dim, hidden_dim, self.input_dim, layer_num = layer_num)     
-#         self.encoder = nn.Linear(self.input_dim,output_dim,bias=False)
-#         self.decoder = nn.Linear(output_dim,self.input_dim,bias=False)
-        self.alpha = alpha
-        self.conv_num = conv_num
-
-        self.output_dim = output_dim
-        self.hidden_dim = hidden_dim
-
-        
-    def msg(self, x, edge_index):
-        x_j = self.propagate(edge_index, x=x)*self.alpha
-        return torch.max(x,x_j) 
-    
-    def forward(self, X, edge_index):
-        x = X
-        edge_index = edge_index
-        x = self.encoder(x)
-        for i in range(self.conv_num):
-            x = self.msg(x, edge_index)
-        return x
